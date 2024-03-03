63fe4375800b8a1e19103d9c91611c1090797741
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-03 23:59:22 +0630

    [Complete] Chat User

---
+++ DotNet8.ConsoleAppChatMessageUsingMediatrPattern/ChatUser.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+public class ChatUser
+{
+    private readonly IMediator _mediator;
+
+    public string Name { get; }
+
+    public ChatUser(IMediator mediator, string name)
+    {
+        _mediator = mediator;
+        this.Name = name;
+    }
+
+    public async Task SendMessage(string message)
+    {
+        await _mediator.Send(new ChatMessage(Name, message));
+    }
+
+    public void ReceiveMessage(string message, string sender)
+    {
+        Console.WriteLine($"{Name} received message from {sender}: {message}");
+    }
+}
