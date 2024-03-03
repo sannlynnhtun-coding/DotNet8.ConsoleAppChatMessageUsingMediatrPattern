7b7ad6275ac10fec361a3dd9ba86086bfea0ea5b
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-03-04 00:00:28 +0630

    [Complete] ChatMessageHandler

---
+++ DotNet8.ConsoleAppChatMessageUsingMediatrPattern/ChatMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+
+public class ChatMessageHandler : IRequestHandler<ChatMessage>
+{
+    private readonly UserRegister _userRegister;
+
+    public ChatMessageHandler(UserRegister userRegister)
+    {
+        Console.WriteLine("Constructing chat message handler");
+        _userRegister = userRegister;
+    }
+
+    public Task Handle(ChatMessage request, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Entering message handler...");
+
+        // NOTE: this code isn't thread-safe in that someone in the future
+        // could be trying to register a user while we're enumerating
+        foreach (var user in _userRegister.Users)
+        {
+            if (user.Name != request.Sender)
+            {
+                user.ReceiveMessage(request.Content, request.Sender);
+            }
+        }
+
+        Console.WriteLine("Exiting message handler.");
+        return Task.CompletedTask;
+    }
+}
